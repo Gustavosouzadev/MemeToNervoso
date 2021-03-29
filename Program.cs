d921b64b86183b6e2dd77c8fb1ce1e3bdee16415
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-29 01:33:29 -0300

    Finalizando MemeToNervoso

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string texto;
+
+            Console.Write("Usuário, o que você pensa sobre mim? ");
+            texto = Console.ReadLine();
+            Console.WriteLine("");
+            
+            Console.WriteLine("- E sabe o que eu penso de você? ");
+            Console.Write("Você é mui");
+            Console.Write("... NÃO, PERA. \nTô nervoso");
+        }
+    }
+}
